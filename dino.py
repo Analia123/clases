26d5f5622d107d19902ed599016aa857b68cc5ec
Labdoo User <anitabritez03@gmail.com>
2019-02-06 17:19:59 -0300

    archivo subido

---
+++ dino.py
@@ -0,0 +1,26 @@
+class Dino:
+    #cola, dientes, color, size 
+    def __init__(self,nombre,color):
+        print("Hola soy un dinosaurio, me llamo", nombre, "y soy de color", color)
+
+pepito= Dino("Pepe, Azul")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
