ee64a89eb18f28b41390d7d5bc2c4b8719ce615e
Labdoo User <anitabritez03@gmail.com>
2019-02-06 17:53:41 -0300

    jajaja

---
+++ persona.py
@@ -0,0 +1,20 @@
+class persona:
+    #nombre, edad, profesion 
+    def __init__(self,un_nombre,una_edad,una_profesion):
+        self.edad= un_edad
+        self.nombre=un_nombre
+        self.profesion=una_profesion
+        print("Hola, yo me llamo", self.nombre,"tengo",self.edad,"anhos y soy",self.profesion)
+
+
+personita=persona("Beatriz","26","programadora")
+personita2=persona("Barbie","32","de todo")
+
+print("holi")
+
+
+
+
+
+
+
