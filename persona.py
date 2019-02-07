9d879e7bf66be501b21f3b3162c978b0e7548ad3
Labdoo User <anitabritez03@gmail.com>
2019-02-07 11:37:22 -0300

    Archivo modificado

--- persona.py
+++ persona.py
@@ -1,20 +1,25 @@
 class persona:
     #nombre, edad, profesion 
     def __init__(self,un_nombre,una_edad,una_profesion):
-        self.edad= un_edad
+        self.edad= una_edad
         self.nombre=un_nombre
         self.profesion=una_profesion
         print("Hola, yo me llamo", self.nombre,"tengo",self.edad,"anhos y soy",self.profesion)
+    def cumpleanhos(self):
+        self.edad= self.edad + 1
+        print("Barbie cumplira", self.edad)
+
+#personita=persona("Beatriz", 26 ,"programadora")
+personita2=persona("Barbie",32,"de todo")
 
 
-personita=persona("Beatriz","26","programadora")
-personita2=persona("Barbie","32","de todo")
 
-print("holi")
-
+#Agregar un metodo a la clase persona que se llame cumpleanhos 
+#y que aumente la edad de la persona en un anho y retorne la edad nueva 
 
 
 
+personita2=persona("barbie", 32, "profesora")
 
 
 
